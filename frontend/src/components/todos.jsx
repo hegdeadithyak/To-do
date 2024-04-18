32c960bdfb6c176d8651baa3579c23defea060c8
hegdeadithyak <hegdeadithyak@gmail.com>
2024-04-19 01:54:01 +0530

    Mark as Complete Support

--- frontend/src/components/todos.jsx
+++ frontend/src/components/todos.jsx
@@ -1,21 +1,31 @@
-import PropTypes from 'prop-types';
-
-Todos.propTypes = {
-    todos: PropTypes.array.isRequired
-};
+async function completeTodo({ todo }) {
+    const response = await fetch("http://localhost:5000/completed", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: todo,
+        }),
+    });
+    const data = await response.json();
+    console.log(data);
+}
 
 export function Todos({ todos }) {
-    return <div>
-        {todos.map(function (todo, index) {
-            return (
-                <div key={index}>
-                    <h1>{todo.title}</h1>
-                    <h2>{todo.description}</h2>
-                    {/* <button onClick={completeTodo}>Complete</button> */}
-                </div>
-            );
-        })}
-    </div>
+    return (
+        <div>
+            {todos.map(function (todo, index) {
+                return (
+                    <div key={index}>
+                        <h1>{todo.title}</h1>
+                        <h2>{todo.description}</h2>
+                        <button onClick={() => completeTodo({ todo })}>Complete</button>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 // function completeTodo({todo}){
